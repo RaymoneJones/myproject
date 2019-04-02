3c6a3d1b9b5bd37a8a7de6f1206ffd408f243c7e
Raymone Jones <160400411@stu.hit.edu.cn>
2019-04-03 00:01:45 +0800

    home page iframe php document

---
+++ home.php
@@ -0,0 +1,93 @@
+<?php
+session_start();
+include("./conn/conn.php")
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <title></title>
+
+</head>
+
+<body >
+    <div style="height: 100%">
+        <div class="left" style="height:100%">
+            <div id="newstop">
+                新闻动态
+                <div style="float: right;font-size:15px;margin-top:15px">
+                    <a href="#">更多>>>></a>
+                </div>
+            </div>
+            <div id="newsblock">
+                <ul>
+                    <?php
+                    $con=mysqli_connect("localhost","root","123456","test");
+                    $sql="select title,time from tb_news  order by time desc limit 10";
+                    $result=mysqli_query($con,$sql);
+                    $num=10;
+                    if($num--){
+                        while($row=$result->fetch_assoc()){
+                            echo '<li><a class="tt" target="_blank" href="#">'.$row['title'].'</a>&nbsp;&nbsp;&nbsp;&nbsp;'.$row['time'].'</li>';
+                        }
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+        <div class="right" style="height:100%">
+            <div class="right1">
+                <div id="noticetop">
+                    通知公告
+                    <div style="float: right;font-size:15px;margin-top:15px">
+                        <a href="#">更多>>>></a>
+                    </div>
+                </div>
+                <div id="noticeblock">
+                    <ul>
+                        <?php
+                        $con=mysqli_connect("localhost","root","123456","test");
+                        $sql="select * from tb_pcard  order by pcard_id desc limit 10";
+                        $result=mysqli_query($con,$sql);
+                        $num=10;
+                        if($num--){
+                            while($row=$result->fetch_assoc()){
+                                echo '<li><a class="tt" target="_blank" href="#">'.$row['pcard_subject'].'</a></li>';
+                            }
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="right2" style="margin-top: 2px">
+                <div id="noticetop">
+                    通知公告
+                    <div style="float: right;font-size:15px;margin-top:15px">
+                        <a href="#">更多>>>></a>
+                    </div>
+                </div>
+                <div id="noticeblock">
+                    <ul>
+                        <?php
+                        $con=mysqli_connect("localhost","root","123456","test");
+                        $sql="select * from tb_pcard  order by pcard_id desc limit 10";
+                        $result=mysqli_query($con,$sql);
+                        $num=10;
+                        if($num--){
+                            while($row=$result->fetch_assoc()){
+                                echo '<li><a class="tt" target="_blank" href="#">'.$row['pcard_subject'].'</a></li>';
+                            }
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </div>
+
+</body>
+</html>
